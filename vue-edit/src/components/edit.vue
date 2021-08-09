bc618d358ec43702433ac6ac8e91b5bc18999506
Aaron peng <aaron.peng@lifebyte.io>
2021-08-09 18:41:41 +0800

    组件传值

---
+++ vue-edit/src/components/edit.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="edit">
+    <div class="edit-list">
+      <button>运行</button>
+      <button>清空</button>
+      {{code}}
+    </div>
+    <div class="edit-box">
+      <textarea :value = "code" @input="handleInput" @keydown.9.prevent="handleKeyDown"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {code:''}
+    },
+    methods:{
+        handleInput(e){
+            this.code = e.target.value
+
+        },
+        handleKeyDown(){
+
+        }
+    }
+};
+</script>
+
+<style lang="stylus">
+.edit {
+  .edit-list {
+    padding: 10px;
+
+    button {
+      height: 40px;
+      width: 80px;
+      margin: 5px;
+    }
+
+    background: #ccc;
+  }
+
+  .edit-box {
+    position: absolute;
+    top: 80px;
+    bottom: 0;
+    left: 0;
+    right:0
+
+    textarea {
+      border: none;
+      outline: none;
+      width: 50%;
+      height: 100%;
+      font-size 24px;
+    }
+  }
+}
+</style>
---
+++ vue-edit/vue.config.js
@@ -0,0 +1,7 @@
+//webpack的配置文件
+// commonjs 语法
+
+module.exports = {
+    runtimeCompiler:true,
+
+}
